03a76d2265284f9bce95c50f6958212cff7c5972
michael-bambha <bambha.m@northeastern.edu>
2025-07-16 15:48:40 -0600

    setup changes, readme changes

---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+
+setup(
+    name="protisplice",
+    version="0.1.0",
+    author="Michael Bambha",
+    author_email="bambha.m@northeastern.edu",
+    description="A Python package for extracting splice site sequences from genomic data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michael-bambha/protisplice",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    python_requires=">=3.8",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "pytest-cov>=2.0",
+            "black>=22.0",
+            "flake8>=4.0",
+            "mypy>=0.900",
+        ],
+        "docs": [
+            "sphinx>=4.0",
+            "sphinx-rtd-theme>=1.0",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "splice-extractor=splice_seq_extractor.scripts.cli:main",
+        ],
+    },
+)
